400ff7b49a8cc6419d768106e65f6bafefce5fb3
dlarson <dlarson@dlarson-pc>
2014-11-25 00:17:33 -0800

    Using BuildConfig for debug log level

--- StockTrackerFragments/src/com/stocktracker/log/Logger.java
+++ StockTrackerFragments/src/com/stocktracker/log/Logger.java
@@ -1,28 +1,23 @@
 package com.stocktracker.log;
 
+import com.stocktracker.BuildConfig;
+
 import android.util.Log;
 import static android.util.Log.DEBUG;
 import static android.util.Log.ERROR;
-import static android.util.Log.INFO;
 import static android.util.Log.WARN;
 
 public class Logger
 {
     private final static String TAG = "StockTracker";
 
-    private final static boolean DEBUG_LOG_ENABLED = false;
-    private final static boolean INFO_LOG_ENABLED = false;
+    private final static boolean DEBUG_LOG_ENABLED = BuildConfig.DEBUG;
     private final static boolean WARN_LOG_ENABLED = true;
     private final static boolean ERROR_LOG_ENABLED = true;
 
     public static boolean isDebugEnabled()
     {
         return DEBUG_LOG_ENABLED;
-    }
-
-    public static boolean isInfoEnabled()
-    {
-        return INFO_LOG_ENABLED;
     }
 
     public static void debug(String message, Object... args)
@@ -60,6 +55,7 @@ public class Logger
         switch(level)
         {
             case DEBUG:
+            {
                 if(DEBUG_LOG_ENABLED)
                 {
                     if(error == null)
@@ -72,20 +68,9 @@ public class Logger
                     }
                 }
                 break;
-            case INFO:
-                if(INFO_LOG_ENABLED)
-                {
-                    if(error == null)
-                    {
-                        Log.i(tag, String.format(message, args));
-                    }
-                    else
-                    {
-                        Log.i(tag, String.format(message, args), error);
-                    }
-                }
-                break;
+            }
             case WARN:
+            {
                 if(WARN_LOG_ENABLED)
                 {
                     if(error == null)
@@ -98,7 +83,9 @@ public class Logger
                     }
                 }
                 break;
+            }
             case ERROR:
+            {
                 if(ERROR_LOG_ENABLED)
                 {
                     if(error == null)
@@ -111,6 +98,7 @@ public class Logger
                     }
                 }
                 break;
+            }
         }
     }
 
