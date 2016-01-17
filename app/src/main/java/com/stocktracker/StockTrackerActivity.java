77a41ea64e051e850fbce273e2d9c52a3d3ba0ec
dlarsonks <dlarson@callpod.com>
2016-01-16 17:50:39 -0800

    Removed the nav drawer. Removed some commented code.

--- app/src/main/java/com/stocktracker/StockTrackerActivity.java
+++ app/src/main/java/com/stocktracker/StockTrackerActivity.java
@@ -1,19 +1,14 @@
 package com.stocktracker;
 
 import android.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -30,7 +25,6 @@ public class StockTrackerActivity extends AppCompatActivity
 
     private static final String FRAGMENT_TAGS[] = {StockListFragment.TAG, AddStockDialogFragment.TAG};
 
-    private DrawerLayout mDrawerLayout;
     private DialogFragment mAddStockDialog;
 
     private StockContentProviderFacade dao;
@@ -47,15 +41,7 @@ public class StockTrackerActivity extends AppCompatActivity
 
         final ActionBar supportActionBar = getSupportActionBar();
         if(supportActionBar != null) {
-            supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-            supportActionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        if (navigationView != null) {
-            setupDrawerContent(navigationView);
+            supportActionBar.setDisplayHomeAsUpEnabled(false);
         }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -77,33 +63,6 @@ public class StockTrackerActivity extends AppCompatActivity
     private void addStock(View view) {
         mAddStockDialog = new AddStockDialogFragment();
         mAddStockDialog.show(getFragmentManager(), AddStockDialogFragment.TAG);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.sample_actions, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                menuItem.setChecked(true);
-                mDrawerLayout.closeDrawers();
-                return true;
-            }
-        });
     }
 
     @Override
@@ -133,25 +92,6 @@ public class StockTrackerActivity extends AppCompatActivity
 
         Toast toast = Toast.makeText(getBaseContext(), getString(R.string.saved), Toast.LENGTH_SHORT);
         toast.show();
-    }
-
-    /**
-     * Get the current fragment's tag or null
-     *
-     * @return The current fragment's tag
-     */
-    private String getCurrentFragmentTag() {
-        Fragment f;
-        for (String tag : FRAGMENT_TAGS) {
-            f = getSupportFragmentManager().findFragmentByTag(tag);
-
-            if (DEBUG) Log.d(TAG, "Fragment tag found '%s', isVisible? = " + (f != null && f.isVisible()));
-
-            if (f != null && f.isVisible()) {
-                return tag;
-            }
-        }
-        return null;
     }
 
     /**
